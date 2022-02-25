f299ca8bc911ec1572e8887e15c170963e8cf692
hugoresende27 <Hugoresende27@gmail.com>
2022-02-25 23:11:01 +0000

    't'

--- resources/views/layouts/mainapp.blade.php
+++ resources/views/layouts/mainapp.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ URL('css/my_styles.css') }}">
+    <link rel="stylesheet" href="css/my_styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 
     <title>Auto Sys</title>
