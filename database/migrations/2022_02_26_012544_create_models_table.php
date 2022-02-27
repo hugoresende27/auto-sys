f4e851be6740ddf0a85e1b512af7e9275898ced0
hugoresende27 <Hugoresende27@gmail.com>
2022-02-27 16:10:36 +0000

    't'

--- database/migrations/2022_02_26_012544_create_models_table.php
+++ database/migrations/2022_02_26_012544_create_models_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('modelos', function (Blueprint $table) {
             $table->id();
-            $table->string('make_id');
+            $table->string('make_id')->unique();
             $table->foreign('make_id')->references('code')->on('makes');
             $table->string('code');
             $table->string('title');
